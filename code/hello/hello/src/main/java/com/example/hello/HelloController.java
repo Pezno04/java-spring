d7f96bee6ba4e9cac50760a5a13ed979748a1dd0
Pedro Faggian <102665768+Pezno04@users.noreply.github.com>
2026-01-14 14:29:36 +0000

    Hello Spring!

---
+++ code/hello/hello/src/main/java/com/example/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(required = false) String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello World!";
+        }
+        return "Hello " + name + "!";
+    }
+}
