a18c1a9658c5c1b01e415610069b8601ed65eeee
Pedro Faggian <102665768+Pezno04@users.noreply.github.com>
2026-05-01 11:15:05 -0300

    Delete code/auth directory

--- code/auth/auth/src/main/java/com/example/auth/model/user/AuthenticationDTO.java
+++
@@ -1,4 +0,0 @@
-package com.example.auth.model.user;
-
-public record AuthenticationDTO(String email, String password) {
-}
--- code/auth/auth/src/main/java/com/example/auth/model/user/RegisterDTO.java
+++
@@ -1,5 +0,0 @@
-package com.example.auth.model.user;
-
-public record RegisterDTO(String email, String password, UserRole role) {
-
-}
